25bc44a845397f455edfa443b47e987af4b4e394
Qinzi Zhang <zqz20100221@126.com>
2024-08-17 10:56:02 -0400

    new batch jobs

--- batch_job/optimistic.sh
+++ batch_job/optimistic.sh
@@ -11,6 +11,27 @@ module load python3/3.10.12 cuda/12.2
 source env/bin/activate
 python check_env.py
 
-# Default batch job. By default without specifying logging.wandb_project, will not log to wandb.
-python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=5000 dataset.total_batch_size=32 \
-    optimizer.lr_config.lr=12e-4
+# 08/16: large batch adam benchmark
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=3e-4
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=1e-3
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=3e-3
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=1e-2
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=3e-2
+
+# 08/16: adam benchmark with random scaling
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=3e-4 train.random_scaling=exponential
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=1e-3 train.random_scaling=exponential
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=3e-3 train.random_scaling=exponential
+# python train_jax.py logging.wandb_project=optimistic_o2nc train.max_steps=2000 dataset.total_batch_size=128 \
+#     optimizer.lr_config.lr=1e-2 train.random_scaling=exponential
+
+
+# 08/17: large batch optimistic adam (ftrl + adagrad lr)
