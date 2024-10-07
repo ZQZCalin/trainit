345748f7db45c7fa761cc2c525a975d453779848
Qinzi Zhang <zqz20100221@126.com>
2024-10-07 17:54:33 -0400

    use stable jax version

--- scc_setup.sh
+++ scc_setup.sh
@@ -7,5 +7,5 @@ module load python3/3.10.12 cuda/12.2
 source env/bin/activate
 pip install -r requirements.txt
 
-# manually download jax to match cuda version
-pip install --upgrade "jax[cuda12]"
+# manually download jax to match cuda version; Updated 10/07: fix to stable version v0.4.31
+pip install --upgrade "jax[cuda12]==0.4.31"
