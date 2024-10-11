c4938ea149b3c5acabb525d25390d3dc153eddb7
Qinzi Zhang <zqz20100221@126.com>
2024-10-10 20:01:25 -0400

    coupled spectrum computation

--- test_hessian.py
+++ test_hessian.py
@@ -90,56 +90,130 @@ def test_power_iteration():
     return
 
 
-def power_iteration_back_prop(
+class SpectrumState(NamedTuple):
+    max_eigenvalue: Array
+    max_eigenvector: Array
+    min_eigenvalue: Array
+    min_eigenvector: Array
+
+
+def coupled_power_iteration(
     train_state: TrainState,
     batches: MiniBatch,
     config: DictConfig,
-    spectrum: int = 0,
     iter: int = 100,
     logger: Optional[RateLimitedWandbLog] = None,
-):
-    """Power iteration modified for back_prop and train_state."""
+) -> SpectrumState:
+    """Power iteration modified for back_prop and train_state.
+    
+    Computes the pair of maximum and minimum eigen-pairs.
+    """
     key, new_key = jr.split(train_state.train_key)
-    x = eqx.filter(train_state.model, eqx.is_array)
-    v = utils.random_unit_vector(x, key=key)
+    params = eqx.filter(train_state.model, eqx.is_array)
+    delta = utils.random_unit_vector(params, key=key)
 
-    def get_grad_f(train_state):
+    spectrum_state = SpectrumState(
+        max_eigenvalue=jnp.zeros([]),
+        max_eigenvector=delta,
+        min_eigenvalue=jnp.zeros([]),
+        min_eigenvector=delta,
+    )
+    
+    def hessian(train_state, x, v, a, b):
+        """Computes Jacobian vector product of (bI+aH)*v where H is hessian at x.
+        Defaults to a=1 and b=0 (i.e., the actual hessian). Set a=-1 and b=lambda_max for the other eigen-pair.
+        """
         def grad_f(x):
             _train_state = train_state._replace(model=eqx.combine(x, train_state.model))
-            _train_state, loss, acc, grads = back_prop(_train_state, batches, config)
-            # Minimum eigen-pair.
-            if spectrum == -1:
-                max_eigenvalue = 6  # TODO: specify in argument
-                grads = jtu.tree_map(
-                    lambda g, _x: max_eigenvalue * _x - g, grads, x
-                )
-            return grads, _train_state
-        return grad_f
-    
-    def hessian(train_state, x, v):
-        return jax.jvp(get_grad_f(train_state), (x,), (v,), has_aux=True)
-    jit_hessian = eqx.filter_jit(hessian)
+            _train_state, _, _, grads = back_prop(_train_state, batches, config)
+            res = jtu.tree_map(
+                lambda g, _x: a*g + b*_x, grads, x
+            )
+            return res, _train_state
+        _, v, train_state = jax.jvp(grad_f, (x,), (v,), has_aux=True)
+        return train_state, v
 
+    # First eigen-pair.
+    if config.logging.wandb_project is not None:
+        wandb.init(project=config.logging.wandb_project)
+        wandb.config.update(OmegaConf.to_container(config))
+    jit_hessian = eqx.filter_jit(
+        jtu.Partial(hessian, a=1, b=0)
+    )
+    v_prev = delta
     pbar = tqdm.tqdm(range(iter), total=iter)
-    last_v = v
     for it in pbar:
-        _, v, train_state = jit_hessian(train_state, x, v)
-        eigenvalue = utils.tree_norm(v)
+        train_state, v = jit_hessian(train_state, params, v_prev)
+        lam = utils.tree_norm(v)
         v = utils.tree_normalize(v)
-        pbar.set_description(f"iter {it+1}/{iter}: eigen-value={eigenvalue}")
+        similarity = utils.tree_cosine_similarity(v, v_prev)
+        v_prev = v
+        pbar.set_description(f"iter {it+1}/{iter}: eigen-value={lam}")
         # Log to wandb.
         metrics = {
-            "eigenvalue": eigenvalue,
-            "cos(vn,vn-1)": utils.tree_cosine_similarity(v, last_v)
+            "eigenvalue": lam,
+            "cos(vn,vn-1)": similarity,
         }
-        last_v = v
         if logger is not None:
             logger(
                 metrics,
                 step=it,
             )
+    if similarity > 0:                              # positive max-eigenvalue
+        spectrum_state = spectrum_state._replace(
+            max_eigenvalue=lam,
+            max_eigenvector=v,
+        )
+        a = -jnp.ones([])
+    else:                                           # negative min-eigenvalue
+        spectrum_state = spectrum_state._replace(
+            min_eigenvalue=-lam,
+            min_eigenvector=v,
+        )
+        a = jnp.ones([])
+    b = lam + 1
+    wandb.finish()
 
-    return eigenvalue, v
+    # Second eigen-pair.
+    if config.logging.wandb_project is not None:
+        wandb.init(project=config.logging.wandb_project)
+        wandb.config.update(OmegaConf.to_container(config))
+    jit_hessian = eqx.filter_jit(
+        jtu.Partial(hessian, a=a, b=b)
+    )
+    v_prev = delta
+    pbar = tqdm.tqdm(range(iter), total=iter)
+    for it in pbar:
+        train_state, v = jit_hessian(train_state, params, v_prev)
+        lam = utils.tree_norm(v)
+        lam = (lam - b) * a
+        v = utils.tree_normalize(v)
+        similarity = utils.tree_cosine_similarity(v, v_prev)
+        v_prev = v
+        pbar.set_description(f"iter {it+1}/{iter}: eigen-value={lam}")
+        # Log to wandb.
+        metrics = {
+            "eigenvalue": lam,
+            "cos(vn,vn-1)": similarity,
+        }
+        if logger is not None:
+            logger(
+                metrics,
+                step=it,
+            )
+    if similarity > 0:                              # positive max-eigen
+        spectrum_state = spectrum_state._replace(
+            max_eigenvalue=lam,
+            max_eigenvector=v,
+        )
+    else:
+        spectrum_state = spectrum_state._replace(
+            min_eigenvalue=lam,
+            min_eigenvector=v,
+        )
+    wandb.finish()
+
+    return spectrum_state
 
 
 def get_batches(dataloader, idx: int, num_batches: int, use_shift_labels: bool):
@@ -153,36 +227,72 @@ def get_batches(dataloader, idx: int, num_batches: int, use_shift_labels: bool):
     return batches
 
 
+# USE \nabla f(x+v) - \nabla f(x) = H*v (finite approximation instead of exact jvp).
+
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(config: DictConfig) -> None:
     """A systematic testing on stochastic loss local landscape."""
-    # manually prevent overwriting test config
+    # For test purpose only, fix some configurations.
+    # Pre-loading presets.
     test_config = config.test
+    config.checkpoint.overwrite_config = False
     config = init_config(config)
+    # Post-loading changes.
     config.test = test_config
     config.logging.wandb_project = "hessian_spectrum"
-    # config.logging.wandb_project = None
-    # logging.info(OmegaConf.to_yaml(config))
+    config.train.use_amp = False
+    logging.info(OmegaConf.to_yaml(config))
 
     train_state, optimizer, train_loader, tokenizer, limited_log = init_train_state(config)
-
-    # [Optional] uncomment to turn off AMP (this must be after loading the checkpoint)
-    config.train.use_amp = False
-
-    # Log to wandb.
-    if config.logging.wandb_project is not None:
-        wandb.init(project=config.logging.wandb_project, name=config.logging.wandb_name)
-        wandb.config.update(OmegaConf.to_container(config))
 
     # Testing variables.
     num_batches = config.dataset.total_batch_size // config.dataset.batch_size
     idx = train_state.iteration * num_batches
     batches = get_batches(train_loader, idx, num_batches, config.dataset.shift_labels)
 
-    spectrum = -1
-    func = jtu.Partial(power_iteration_back_prop, batches=batches, config=config, iter=100, logger=limited_log, spectrum=spectrum)
-    eigenvalue, eigenvector = func(train_state)
-    # print(f"eigen-value: {eigenvalue}, eigen-vector: ", eigenvector)
+    # Temporary tests
+    updates = train_state.aux_state.params_diff
+    spectrum_state = SpectrumState(
+        max_eigenvalue=jnp.zeros([]), 
+        max_eigenvector=updates,
+        min_eigenvalue=jnp.zeros([]),
+        min_eigenvector=updates,
+    )
+    path = "checkpoint/hessian_spectrum/adam_benchmark/iter_100.ckpt"
+    spectrum_state = serializer.load(path, spectrum_state)
+
+    def hessian(train_state, x, v, a, b):
+        """Computes Jacobian vector product of (bI+aH)*v where H is hessian at x.
+        Defaults to a=1 and b=0 (i.e., the actual hessian). Set a=-1 and b=lambda_max for the other eigen-pair.
+        """
+        def grad_f(x):
+            _train_state = train_state._replace(model=eqx.combine(x, train_state.model))
+            _train_state, _, _, grads = back_prop(_train_state, batches, config)
+            res = jtu.tree_map(
+                lambda g, _x: a*g + b*_x, grads, x
+            )
+            return res, _train_state
+        _, v, train_state = jax.jvp(grad_f, (x,), (v,), has_aux=True)
+        return train_state, v
+
+    x = eqx.filter(train_state.model, eqx.is_array)
+    v = spectrum_state.min_eigenvector
+    _, v_next = hessian(train_state, x, v, a=1, b=0)
+    print(f"max eigenvalue: {spectrum_state.max_eigenvalue}; min: {spectrum_state.min_eigenvalue}")
+    print(f"max norm: {utils.tree_norm(spectrum_state.max_eigenvector)}; min norm: {utils.tree_norm(spectrum_state.min_eigenvector)}")
+    print(utils.tree_norm(v_next), utils.tree_cosine_similarity(v, v_next))
+
+
+    raise KeyboardInterrupt()
+
+    # Spectrum power iteration.
+    spectrum_state = coupled_power_iteration(
+        train_state, batches=batches, config=config, logger=limited_log, iter=20
+    )
+    path = config.test.hessian.save_path
+    if not path:
+        path = "checkpoint/hessian_spectrum/adam_benchmark/spectrum.ckpt"
+    serializer.save(path, spectrum_state)
     
 
 if __name__ == "__main__":
