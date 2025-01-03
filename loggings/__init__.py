0910c4eaa66212de8164b1cd54ed5e5f7a52eb99
Qinzi Zhang <zqz20100221@126.com>
2025-01-03 00:25:19 -0500

    log functions

---
+++ loggings/__init__.py
@@ -0,0 +1,8 @@
+"""The loggings subfolder.
+
+Implements the LoggingFn class.
+"""
+
+from loggings.base import LogFn, LogState, LogMetrics
+from loggings.demo import simple_log
+from loggings.demo import full_log
---
+++ loggings/base.py
@@ -0,0 +1,51 @@
+"""A stateless log function"""
+
+import optax
+from typing import Any, Dict, Tuple, NamedTuple, Protocol
+from jaxtyping import Array, PyTree
+from utils import merge_dicts, list_of_logs
+
+
+LogState = PyTree
+LogMetrics = PyTree
+
+
+class LogInitFn(Protocol):
+    def __call__(self, **extra_args: Any) -> LogState:
+        """The `init` function."""
+
+
+class LogUpdateFn(Protocol):
+    def __call__(self, state: LogState, **extra_args: Any) -> Tuple[LogState, LogMetrics]:
+        """The `update` function."""
+
+
+class LogFn(NamedTuple):
+    """A stateless log function implemented by a tuple of `init_fn` and `update_fn`.
+    
+    Log functions should only compute metrics based on input arguments and
+    may *not* incur any extra computations, including forward and backward props.
+    Such computations should be called outside the `update_fn`, whose results are then
+    passed to the log function to compute associated metrics.
+
+    For convenience of use, please include a use example of both `init_fn` and `update_fn`
+    when customizing your own log function.
+
+    Examples:
+        >>> from loggings import base
+        >>> def example_log() -> base.LogFn:
+        ...  '''An example log function.'''
+        ...  # log_fn = example_log()
+        ...  # log_state = log_fn.init(params=...)
+        ...  # log_state, log_metrics = log_fn.update(log_state, params=..., grads=...)
+    """
+    # TODO: this is the only part I don't like about LogFn: users need to write different lines
+    # in the train() function to update different log functions. Ideally, we'd want one uniform
+    # line to achieve this.
+    init: LogInitFn
+    update: LogUpdateFn
+
+
+def get_internal_logs(opt_state: optax.OptState) -> Dict[Array]:
+    """Fetchs `utils.Log` objects, the internal logging metrics, from an `optax.OptState`."""
+    return merge_dicts(*list_of_logs(opt_state))
