ead1cd85efebc348d14bdd0261a7750f75898abc
Qinzi Zhang <zqz20100221@126.com>
2025-01-09 23:29:50 -0500

    Create a generic `adam_base` optimizer that derives other optimizers

--- optimizers/base.py
+++ optimizers/base.py
@@ -15,8 +15,8 @@ import optimizers.schedule as schedule
 ScalarOrPytree = Union[float, PyTree]
 
 
-class AdamState(NamedTuple):
-    """Adam State."""
+class AdamBaseState(NamedTuple):
+    """adam_base state."""
     count: Array
     mu: Optional[optax.Updates]
     nu: Optional[optax.Updates]
@@ -24,22 +24,24 @@ class AdamState(NamedTuple):
     logging: Optional[log_utils.Log]
 
 
-def adam(
-    learning_rate: optax.ScalarOrSchedule = 1e-4,
-    beta1: float = 0.9,
-    beta2: float = 0.999,
-    eps: float = 1e-8,
-    weight_decay: float = 0.0,
-    decouple_weight_decay: bool = True,
-    debias_beta1: bool = True,
-    debias_beta2: bool = True,
-    use_momentum: bool = True,
-    use_momentum_state: bool = True,
-    use_precond: bool = True,
-    use_precond_state: bool = True,
-    use_constant_wd: bool = False,
+def adam_base(
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        beta1: float = 0.9,
+        beta2: float = 0.999,
+        eps: float = 1e-8,
+        weight_decay: float = 0.0,
+        decouple_weight_decay: bool = False,
+        use_nesterov: bool = False,
+        debias_beta1: bool = True,
+        debias_beta2: bool = True,
+        use_momentum: bool = True,
+        use_momentum_state: bool = True,
+        use_precond: bool = True,
+        use_precond_state: bool = True,
+        use_constant_wd: bool = False,
+        logger: Optional[Logger] = None,
 ) -> optax.GradientTransformation:
-    """The Adam optimizer.
+    """The base Adam optimizer.
 
     Implements the Adam optimizer from:
 
@@ -49,9 +51,14 @@ def adam(
     including SGD, SGDM, and RMSProp.
 
     `decouple_weight_decay` controls whether to update as Adam (wd before momentum) 
-    or AdamW (wd after momentum), where AdamW is implemented from:
+    or AdamW (wd after momentum), where AdamW is adapted from:
 
     https://arxiv.org/pdf/1711.05101
+
+    `use_nesterov` controls whether to apply nesterov's accelerated gradient to update,
+    which is adapted from this report:
+
+    https://cs229.stanford.edu/proj2015/054_report.pdf
 
     If users need to apply different lr or wd to different subgroups of the model,
     please use `optax.multi_transform`.
@@ -62,7 +69,8 @@ def adam(
         beta2: momentum constant of adaptive gradients.
         eps: stability constant.
         weight_decay: weight decay.
-        decouple_weight_decay: Defaults to False. If true, learning rate eta will not be applied to weight_decay regularization.
+        decouple_weight_decay: if true, uses adamw update, i.e., apply weight decay after momentum aggregation.
+        use_nesterov: if true, uses NAG update, i.e., slighly overshoots the momentum update with current gradient.
         debias_beta1: if true, uses unbiased EMA of momentum. Defaults to True.
         debias_beta2: if true, uses unbiased EMA of gradient preconditioner. Defaults to True.
         use_momentum: If false, replace \hat m_t with g_t.
@@ -70,7 +78,8 @@ def adam(
         use_precond: If false, does not scale \hat m_t by the adaptive gradient pre-conditioner v_t.
         use_precond_state: if false, does not store v_t in the opt_state.
         use_constant_wd: an experimental parameter. if true, weight decay does not scale with learning rate schedule. Defaults to False.
-
+        logger: logging callback, a `loggers.Logger` object that initializes and updates log_state and log_metrics.
+        
     Returns:
         A `GradientTransformation` object.
     """
@@ -90,7 +99,7 @@ def adam(
             "optimizer/cos(g,m/sqrt(v))": jnp.zeros([]),
             "optimizer/cos(g,g/sqrt(v))": jnp.zeros([]),
         }
-        return AdamState(
+        return AdamBaseState(
             count=jnp.zeros([], jnp.int32),
             mu=jtu.tree_map(jnp.zeros_like, params),
             nu=jtu.tree_map(jnp.zeros_like, params),
@@ -147,61 +156,247 @@ def adam(
             "optimizer/cos(g,m/sqrt(v))": tree_utils.cosine(updates, precond_mu),
             "optimizer/cos(g,g/sqrt(v))": tree_utils.cosine(updates, precond_g),
         }
-        return new_updates, AdamState(
+        return new_updates, AdamBaseState(
             count=count_inc, mu=mu, nu=nu, logging=log_utils.Log(logging))
     
     return optax.GradientTransformation(init_fn, update_fn)
 
 
-class SgdmState(NamedTuple):
-    count: Array
-    momentum: optax.Updates
+def adam(
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        beta1: float = 0.9,
+        beta2: float = 0.999,
+        eps: float = 1e-8,
+        weight_decay: float = 0.0,
+        logger: Optional[Logger] = None,
+    ) -> optax.GradientTransformation:
+    """The Adam optimizer from
+    
+    https://arxiv.org/pdf/1412.6980
+
+    Does not apply nesterov's momentum nor decoupled weight decay.
+    Use nadam or adamw instead for those updates.
+    """
+    return adam_base(
+        learning_rate=learning_rate,
+        beta1=beta1,
+        beta2=beta2,
+        eps=eps,
+        weight_decay=weight_decay,
+        decouple_weight_decay=False,
+        use_nesterov=False,
+        debias_beta1= True,
+        debias_beta2=True,
+        use_momentum=True,
+        use_momentum_state=True,
+        use_precond=True,
+        use_precond_state=True,
+        use_constant_wd=False,
+        logger=logger,
+    )
+
+
+def adamw(
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        beta1: float = 0.9,
+        beta2: float = 0.999,
+        eps: float = 1e-8,
+        weight_decay: float = 0.0,
+        use_nesterov: bool = False,
+        logger: Optional[Logger] = None,
+) -> optax.GradientTransformation:
+    """Implements the AdamW optimizer from
+    
+    https://arxiv.org/pdf/1711.05101
+
+    Nesterov's momentum is default to false, but 
+    you can manually turn it on.
+    """
+    return adam_base(
+        learning_rate=learning_rate,
+        beta1=beta1,
+        beta2=beta2,
+        eps=eps,
+        weight_decay=weight_decay,
+        decouple_weight_decay=True,
+        use_nesterov=use_nesterov,
+        debias_beta1= True,
+        debias_beta2=True,
+        use_momentum=True,
+        use_momentum_state=True,
+        use_precond=True,
+        use_precond_state=True,
+        use_constant_wd=False,
+        logger=logger,
+    )
+
+
+def nadam(
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        beta1: float = 0.9,
+        beta2: float = 0.999,
+        eps: float = 1e-8,
+        weight_decay: float = 0.0,
+        decouple_weight_decay: bool = True,
+        logger: Optional[Logger] = None,
+) -> optax.GradientTransformation:
+    """Implements the Nadam optimizer from
+    
+    https://cs229.stanford.edu/proj2015/054_report.pdf
+
+    Decoupled weight decay is default to true due to the belief
+    that adamw update is more suitable for larger models.
+    However, you can manually turn it off.
+    """
+    return adam_base(
+        learning_rate=learning_rate,
+        beta1=beta1,
+        beta2=beta2,
+        eps=eps,
+        weight_decay=weight_decay,
+        decouple_weight_decay=decouple_weight_decay,
+        use_nesterov=True,
+        debias_beta1= True,
+        debias_beta2=True,
+        use_momentum=True,
+        use_momentum_state=True,
+        use_precond=True,
+        use_precond_state=True,
+        use_constant_wd=False,
+        logger=logger,
+    )
+
+
+def rmsprop(
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        momentum: float = 0.99,
+        eps: float = 1e-8,
+        weight_decay: float = 0.0,
+        decouple_weight_decay: bool = True,
+        logger: Optional[Logger] = None,
+    ) -> optax.GradientTransformation:
+    """The RMSProp optimizer from
+    
+    https://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf
+
+    Decoupled weight decay is default to true.
+    """
+    return adam_base(
+        learning_rate=learning_rate,
+        beta2=momentum,
+        eps=eps,
+        weight_decay=weight_decay,
+        decouple_weight_decay=decouple_weight_decay,
+        use_nesterov=False,
+        debias_beta1= True,
+        debias_beta2=True,
+        use_momentum=False,
+        use_momentum_state=False,
+        use_precond=True,
+        use_precond_state=True,
+        use_constant_wd=False,
+        logger=logger,
+    )
 
 
 def sgdm(
-    learning_rate: optax.ScalarOrSchedule,
-    beta: float=0.0,
-    weight_decay: ScalarOrPytree=0.0,
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        momentum: float = 0.9,
+        weight_decay: float = 0.0,
+        decouple_weight_decay: bool = True,
+        use_nesterov: bool = False,
+        logger: Optional[Logger] = None,
 ) -> optax.GradientTransformation:
-    """SGD with momentum.
+    """The SGD-Momentum optimizer.
     
-    Updates m_{t+1} = beta * m_t - (1-beta) * (g_t + mu*x_t)
-        and x_{t+1} = x_t - eta_t * m_{t+1}, 
-    where beta is the momentum constant and mu is the weight decay constant.
-
-    Args:
-        learning_rate: The learning rate scheduler.
-        beta: The momentum constant in [0, 1]. Defaults to 0.
-        weight_decay (float): The weight decay constant. Defaults to 0.
-
-    Returns:
-        A `GradientTransformation` object.
+    Decoupled weight decay is default to true,
+    and nesterov's momentum is default to false.
     """
-    
-    # use_pytree_wd = type(weight_decay) != float
-    use_pytree_wd = not isinstance(weight_decay, float)
+    return adam_base(
+        learning_rate=learning_rate,
+        beta1=momentum,
+        weight_decay=weight_decay,
+        decouple_weight_decay=decouple_weight_decay,
+        use_nesterov=use_nesterov,
+        debias_beta1= True,
+        debias_beta2=True,
+        use_momentum=True,
+        use_momentum_state=True,
+        use_precond=False,
+        use_precond_state=False,
+        use_constant_wd=False,
+        logger=logger,
+    )
 
-    def init_fn(params):
-        if use_pytree_wd and jtu.tree_structure(weight_decay)!=jtu.tree_structure(params):
-            raise ValueError("structure of weight_decay must match model structure.")
-        return SgdmState(
-            count = jnp.zeros([], jnp.int32),
-            momentum = jtu.tree_map(jnp.zeros_like, params),
-        )
+
+def sgd(
+        learning_rate: optax.ScalarOrSchedule = 1e-4,
+        weight_decay: float = 0.0,
+        logger: Optional[Logger] = None,
+) -> optax.GradientTransformation:
+    """The vanilla SGD optimizer."""
+    return adam_base(
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        use_momentum=False,
+        use_momentum_state=False,
+        use_precond=False,
+        use_precond_state=False,
+        use_constant_wd=False,
+        logger=logger,
+    )
+
+
+# NOTE: we will use adam_base to derive any related optimizers including SGDM.
+# class SgdmState(NamedTuple):
+#     count: Array
+#     momentum: optax.Updates
+
+
+# def sgdm(
+#     learning_rate: optax.ScalarOrSchedule,
+#     beta: float=0.0,
+#     weight_decay: ScalarOrPytree=0.0,
+# ) -> optax.GradientTransformation:
+#     """SGD with momentum.
     
-    def update_fn(updates, state, params):
-        eta = schedule.get_current_lr(learning_rate, state.count)
-        new_momentum = jtu.tree_map(
-            lambda m, g: beta*m + (1-beta)*g, state.momentum, updates)
-        if not use_pytree_wd:
-            new_updates = jtu.tree_map(
-                lambda m, x: -eta * (m + weight_decay*x), new_momentum, params)
-        else:
-            new_updates = jtu.tree_map(
-                lambda m, x, wd: -eta * (m + wd*x), new_momentum, params, weight_decay)
-        return new_updates, SgdmState(
-            count = optax.safe_int32_increment(state.count),
-            momentum = new_momentum
-        )
+#     Updates m_{t+1} = beta * m_t - (1-beta) * (g_t + mu*x_t)
+#         and x_{t+1} = x_t - eta_t * m_{t+1}, 
+#     where beta is the momentum constant and mu is the weight decay constant.
+
+#     Args:
+#         learning_rate: The learning rate scheduler.
+#         beta: The momentum constant in [0, 1]. Defaults to 0.
+#         weight_decay (float): The weight decay constant. Defaults to 0.
+
+#     Returns:
+#         A `GradientTransformation` object.
+#     """
     
-    return optax.GradientTransformation(init_fn, update_fn)
+#     # use_pytree_wd = type(weight_decay) != float
+#     use_pytree_wd = not isinstance(weight_decay, float)
+
+#     def init_fn(params):
+#         if use_pytree_wd and jtu.tree_structure(weight_decay)!=jtu.tree_structure(params):
+#             raise ValueError("structure of weight_decay must match model structure.")
+#         return SgdmState(
+#             count = jnp.zeros([], jnp.int32),
+#             momentum = jtu.tree_map(jnp.zeros_like, params),
+#         )
+    
+#     def update_fn(updates, state, params):
+#         eta = schedule.get_current_lr(learning_rate, state.count)
+#         new_momentum = jtu.tree_map(
+#             lambda m, g: beta*m + (1-beta)*g, state.momentum, updates)
+#         if not use_pytree_wd:
+#             new_updates = jtu.tree_map(
+#                 lambda m, x: -eta * (m + weight_decay*x), new_momentum, params)
+#         else:
+#             new_updates = jtu.tree_map(
+#                 lambda m, x, wd: -eta * (m + wd*x), new_momentum, params, weight_decay)
+#         return new_updates, SgdmState(
+#             count = optax.safe_int32_increment(state.count),
+#             momentum = new_momentum
+#         )
+    
+#     return optax.GradientTransformation(init_fn, update_fn)
