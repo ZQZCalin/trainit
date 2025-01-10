c37d44eff96cb8c9eca76b87054abfd08d1ea475
Qinzi Zhang <zqz20100221@126.com>
2025-01-10 15:54:47 -0500

    update benchmark scripts

--- scripts/baselines/adamw.sh
+++ scripts/baselines/adamw.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 project=pile_baseline
+name=adamw
 log_data=True
 steps=2000
 batch_size=128
@@ -14,8 +15,12 @@ schedule=linear
 lrs=(1e-2 3e-3 1e-3 3e-4 1e-4)
 warmup=200
 
+# System variables
 BASE_DIR=/projectnb/aclab/qinziz/trainit
-DATE=$(date +"%Y/%m/%d")
+DATE=$(date +"%Y-%m-%d")
+OUTPUT_PATH=$BASE_DIR/scc_outputs/$DATE/$name
+
+mkdir -p $OUTPUT_PATH
 
 for lr in "${lrs[@]}"
 do
@@ -26,9 +31,9 @@ do
 #$ -l gpus=1
 #$ -l gpu_type=L40S     # Specifies the gpu type.
 #$ -l h_rt=8:00:00      # Specifies the hard time limit for the job
-#$ -N "$optimizer"_"$lr".sh
-#$ -o $BASE_DIR/scc_outputs/$DATE/$optimizer/$JOB_NAME.o$JOB_ID
-#$ -e $BASE_DIR/scc_outputs/$DATE/$optimizer/$JOB_NAME.e$JOB_ID
+#$ -N "$name"_"$lr".sh
+#$ -o $OUTPUT_PATH/$JOB_NAME.o$JOB_ID
+#$ -e $OUTPUT_PATH/$JOB_NAME.e$JOB_ID
 
 sleep $(((RANDOM % 1000) / 100))   # Prevents simultaneous reads of loadit dataset
 
@@ -36,7 +41,7 @@ source activate_env.sh
 
 python main.py \
     logging.wandb_project=$project \
-    logging.wandb_name="$optimizer"_lr:"$lr" \
+    logging.wandb_name="$name"_lr:"$lr" \
     logging.log_callback_data=$log_data \
     train.max_steps=$steps \
     dataset.total_batch_size=$batch_size \
@@ -48,5 +53,5 @@ python main.py \
     optimizer.lr_config.warmup=$warmup \
     optimizer.lr_config.max_steps=$steps
 EOF
-    echo "Submitted job: $optimizer lr=$lr"
+    echo "Submitted job: $name lr=$lr"
 done
