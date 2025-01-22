21c875af91034cf513ceb13eda1bf608a8562ddf
Qinzi Zhang <zqz20100221@126.com>
2025-01-21 19:11:54 -0500

    update wandb init to make sure run_id is always a str

--- _src/initialize.py
+++ _src/initialize.py
@@ -284,6 +284,7 @@ def init_wandb(config: DictConfig) -> None:
         wandb.init(
             project=config.logging.wandb_project, 
             name=config.logging.wandb_name,
-            id=config.logging.wandb_runid,
+            id=str(config.logging.wandb_runid),
+            resume="never",
         )
         wandb.config.update(OmegaConf.to_container(config))
