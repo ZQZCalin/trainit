eb23ccbaf1078c5e10426d7083b87e01658dbbe5
Qinzi Zhang <zqz20100221@126.com>
2025-01-28 14:41:15 -0500

    updates `get_next_lr` and implements default lrs

--- scripts/schedule/get_next_lr.py
+++ scripts/schedule/get_next_lr.py
@@ -13,11 +13,17 @@ import numpy as np
 from typing import Any
 
 
-# Type of smoothing. you can implement your own way of smoothing.
+# >> Type of smoothing. you can implement your own way of smoothing.
 SMOOTHING = "EMA"
 EMA_WINDOW_SIZE = 10
 
-# Next lr methods.
+
+# >> Default lrs.
+DEFAULT_LR1 = 0.0
+DEFAULT_LR2 = [1e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+
+
+# >> Next lr methods.
 NEXT_LR1 = "eps_greedy"
 EPS_GREEDY_VAL = 0.01
 
@@ -25,9 +31,15 @@ NEXT_LR2 = "log"
 LOG_GRID_MULTI = 2
 LOG_GRID_SIZE = 2       # additional lrs on each side
 
-# Other global variables
+
+# >> Other global variables
+# Wandb team/organization name.
 WANDB_ENTITY = "optimizedlearning"
 
+
+# =========================================================
+# >>> SMOOTHING METHODS
+# =========================================================
 
 def EMA(losses: list) -> list:
     """Applies EMA smoothing."""
@@ -39,6 +51,72 @@ def EMA(losses: list) -> list:
 def customized_smoothing(loss: list) -> list:
     raise NotImplementedError
 
+
+# =========================================================
+# >>> DEFAULT LR SETTUP
+# =========================================================
+
+def get_default_lr1() -> float:
+    """Returns default value of lr1."""
+    return DEFAULT_LR1
+
+
+def get_default_lr2() -> list:
+    """Returns default value of lr2."""
+    return DEFAULT_LR2
+
+
+# =========================================================
+# >>> LR1 NEXT SEGMENT
+# =========================================================
+
+def greedy_lr1(arr: np.ndarray) -> float:
+    """Returns lr with lowest loss value."""
+    i = np.argmin(arr[:, 1])
+    return arr[i, 0]
+
+
+def eps_greedy_lr1(arr: np.ndarray) -> float:
+    """Returns largest lr such that loss <= loss_min + eps."""
+    loss_min = np.min(arr[:, 1])
+    arr_filtered = arr[arr[:, 1] <= loss_min + EPS_GREEDY_VAL]
+    return np.max(arr_filtered[:, 0])
+
+
+# Customize your own lr mechanism if needed.
+def customized_lr1(arr: np.ndarray) -> float:
+    raise NotImplementedError
+
+
+# =========================================================
+# >>> LR2 NET SEGMENT
+# =========================================================
+
+def loggrid_lr2(val: float) -> list:
+    """list of lr2 with log-grid search, including 0."""
+    # Special case: if val == 0, returns default setting
+    if val == 0:
+        res = get_default_lr2()
+        if 0.0 not in res:
+            res.append(0.0)
+        return sorted(res)
+    
+    # Standard case.
+    res = [val, 0.0]
+    lr = val
+    for _ in range(LOG_GRID_SIZE):
+        lr *= LOG_GRID_MULTI
+        res.append(lr)
+    lr = val
+    for _ in range(LOG_GRID_SIZE):
+        lr /= LOG_GRID_MULTI
+        res.append(lr)
+    return sorted(res)
+
+
+# =========================================================
+# >>> WRAPPER FUNCTIONS
+# =========================================================
 
 def smoothing(losses: list) -> list:
     """Wraps all smoothing mechanisms."""
@@ -61,40 +139,20 @@ def get_run_info(run: Any) -> tuple[float, float]:
     return lr2, last_loss
 
 
-def greedy_lr1(arr: np.ndarray) -> float:
-    """Returns lr with lowest loss value."""
-    i = np.argmin(arr[:, 1])
-    return arr[i, 0]
-
-
-def eps_greedy_lr1(arr: np.ndarray) -> float:
-    """Returns largest lr such that loss <= loss_min + eps."""
-    loss_min = np.min(arr[:, 1])
-    arr_filtered = arr[arr[:, 1] <= loss_min + EPS_GREEDY_VAL]
-    return np.max(arr_filtered[:, 0])
-
-
-# Customize your own lr mechanism if needed.
-def customized_lr1(arr: np.ndarray) -> float:
-    raise NotImplementedError
-
-
-def log_lr2(val: float) -> list:
-    """list of lr2 with log-grid search, including 0."""
-    res = [val, 0.0]
-    lr = val
-    for _ in range(LOG_GRID_SIZE):
-        lr *= LOG_GRID_MULTI
-        res.append(lr)
-    lr = val
-    for _ in range(LOG_GRID_SIZE):
-        lr /= LOG_GRID_MULTI
-        res.append(lr)
-    return sorted(res)
-
-
 def get_next_lrs(arr: np.ndarray) -> tuple[float, list]:
-    """Wraps all lr methods."""
+    """Wraps all lr methods.
+    
+    Args:
+        arr: [n, 2] array, each row is (lr2, last_loss)
+    
+    Returns:
+        A tuple of (lr1, lr2_candidates)
+    """
+    # Special case: initialize when arr is empty.
+    if len(arr) == 0:
+        return get_default_lr1(), get_default_lr2()
+    
+    # Get lr1.
     if NEXT_LR1 == "greedy":
         lr1 = greedy_lr1(arr)
     elif NEXT_LR1 == "eps_greedy":
@@ -102,8 +160,10 @@ def get_next_lrs(arr: np.ndarray) -> tuple[float, list]:
     # Add your customized methods below.
     else:
         raise ValueError(f"unsupport lr1 mechanism = '{NEXT_LR1}'.")
+    
+    # Get lr2.
     if NEXT_LR2 == "log":
-        lr2_candidates = log_lr2(lr1)
+        lr2_candidates = loggrid_lr2(lr1)
     # Add your customized methods below.
     else:
         raise ValueError(f"unsupport lr2 mechanism = '{NEXT_LR2}'.")
@@ -118,7 +178,7 @@ def main():
     )
     parser.add_argument(
         "--job_ids",
-        nargs="+",
+        nargs="*",
         type=int,
     )
     args = parser.parse_args()
