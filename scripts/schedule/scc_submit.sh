50632dca67d9b42c4b96bc996ab74d6b4bad86c1
Qinzi Zhang <zqz20100221@126.com>
2025-01-28 14:43:01 -0500

    cleans up redundant variables

--- scripts/schedule/scc_submit.sh
+++ scripts/schedule/scc_submit.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 source scripts/schedule/config.sh
-OUTPUT_PATH="${BASE_PATH}/scc_outputs/${DATE}/${NAME}"
 
-mkdir -p "$OUTPUT_PATH"
+mkdir -p "$SCC_OUTPUT_PATH"
 
 qsub -l h_rt="$CPU_HOUR" \
     -N "${NAME}_master" \
-    -o "${OUTPUT_PATH}/master.o\$JOB_ID" \
-    -e "${OUTPUT_PATH}/master.e\$JOB_ID" \
-    -v CPU_HOUR,NAME,OUTPUT_PATH \
+    -o "${SCC_OUTPUT_PATH}/master.o\$JOB_ID" \
+    -e "${SCC_OUTPUT_PATH}/master.e\$JOB_ID" \
+    -v CPU_HOUR,NAME,SCC_OUTPUT_PATH \
     scripts/schedule/master.sh
 
 echo "Submitted the master script for experiment ${NAME}."
