0f1b122d089e3bc3fc0f9deb57a86e3ad0cd8c83
Qinzi Zhang <zqz20100221@126.com>
2025-01-28 14:41:42 -0500

    change all variables to static variables

--- scripts/schedule/config.sh
+++ scripts/schedule/config.sh
@@ -1,12 +1,11 @@
-# Global configuration variables
+# Static configuration variables
 
 # PLEASE USE A NEW NAME FOR EVERY NEW EXPERIMENT!
-NAME="eps-greedy_10segs(2)"
+NAME="test_v2_trial1"
 DESC="
 Experiment description:
 
-2000 steps split into 10 segments, with eps-greedy mechanism.
-Trial 2 after adding the error catcher.
+First trial of testing the new automation script after adding the resubmit feature.
 "
 
 # =========================================================
@@ -18,64 +17,22 @@ BASE_PATH="/projectnb/aclab/qinziz/trainit"
 
 DATE=$(date +"%Y-%m-%d")
 
-# Total cpu hour of the master script.
+# total cpu hour of the master script
 CPU_HOUR="24:00:00"
 
 GPU_TYPE="L40S"
 
-# GPU hour per parallel job.
+# GPU hour per parallel job
 GPU_HOUR="4:00:00"
 
+# path of scc output files
+SCC_OUTPUT_PATH="${BASE_PATH}/scc_outputs/${DATE}/${NAME}"
 
-# =========================================================
-# >>> EXPERIMENT
-# =========================================================
+# path of checkpoint files
+CHECKPOINT_PATH="checkpoint/lr_schedule/${NAME}"
 
-# maximum number of training steps
-total_steps=2000
-
-# number of segments
-num_segments=10
-
-# list of checkpoint iterations
-#   i. you can use evenly distributed segments by changing `num_segments`
-segments=( $(seq 0 $((total_steps/num_segments)) $total_steps) )
-segments[-1]=$total_steps           # set last segment to total_steps
-
-#   ii. alternatively, you can customize unevenly distributed segments
-# segments=(0 150 300 500 1000 1500 $total_steps)
-
-# learning rates for the first segment
-lr1=0
-lr2_candidates=(1e0 1e-1 1e-2 1e-3 1e-4 1e-5)
-
-
-# >>> Other training configs
-
-# global random seed
-random_seed=42
-
-# training batch size
-batch_size=128
-
-# optimizer configs
-optimizer=adamw
-beta1=0.9
-beta2=0.999
-weight_decay=0.1
-nesterov=False
-
-# checkpoint subfolder relative path
-checkpoint_path="checkpoint/lr_schedule/${NAME}"
-
-
-# >>> Logging configs
-
-# wandb project name
-project="greedy_lr_schedule"
-
-# log additional metrics to wandb
-log_callback_data=True
+# delete checkpoints of suboptimal runs
+CLEAN_CHECKPOINTS=True
 
 
 # =========================================================
@@ -83,15 +40,77 @@ log_callback_data=True
 # =========================================================
 
 # master host ip address
-master_host=$(hostname -I | awk '{print $1}')
+MASTER_HOST=$(hostname -I | awk '{print $1}')
 
 # port number for communication
-port=51204
+PORT=51204
+
+# backoff time (in seconds) between listener attempts
+LISTENER_BACKOFF=5
 
 # maximum wait time of listener (in seconds)
 #   NOTE: a job still sends ACK token if the python job
 #   throws an error.
-#   currently set to 4 hours. feel free to change
-max_wait_mins=240
-max_wait_time=$((max_wait_mins*60))
-# max_wait_time=10    # uncomment it for test purpose
+MAX_LISTEN_TIME=14400     # 4 hours
+# MAX_LISTEN_TIME=10      # uncomment it for test purpose
+
+
+# =========================================================
+# >>> RESUBMIT
+# =========================================================
+
+# Maximum number of retry attempts per job
+MAX_RETRIES=3
+
+# Backoff time (in seconds) between retry attempts
+RETRY_BACKOFF=60
+
+# Optional: Enable or disable resubmission feature
+ENABLE_RETRY=true
+
+
+# =========================================================
+# >>> EXPERIMENT
+# =========================================================
+
+# maximum number of training steps
+TOTAL_STEPS=2000
+
+# number of segments
+NUM_SEGMENTS=10
+
+# list of checkpoint iterations
+#   i. you can use evenly distributed segments by changing `num_segments`
+SEGMENTS=( $(seq 0 $((TOTAL_STEPS/NUM_SEGMENTS)) $TOTAL_STEPS) )
+SEGMENTS[-1]=$TOTAL_STEPS           # set last segment to TOTAL_STEPS
+
+#   ii. alternatively, you can customize unevenly distributed segments
+# SEGMENTS=(0 150 300 500 1000 1500 $TOTAL_STEPS)
+
+
+# >>> Other training configs
+
+# global random seed
+RANDOM_SEED=42
+
+# training batch size
+BATCH_SIZE=128
+
+# optimizer configs
+OPTIMIZER=adamw
+BETA1=0.9
+BETA2=0.999
+WEIGHT_DECAY=0.1
+NESTEROV=False
+
+# checkpoint subfolder relative path
+CHECKPOINT_PATH="checkpoint/lr_schedule/${NAME}"
+
+
+# >>> Logging configs
+
+# wandb project name
+PROJECT="greedy_lr_schedule"
+
+# log additional metrics to wandb
+LOG_CALLBACK_DATA=True
