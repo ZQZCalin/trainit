b72f2f51df41ad75e5028ac826b6c259be480ec5
Qinzi Zhang <zqz20100221@126.com>
2025-02-07 12:02:36 -0500

    tests `muon_laprop`

--- optimizers/muon/muon_test.py
+++ optimizers/muon/muon_test.py
@@ -8,7 +8,7 @@ import equinox as eqx
 from omegaconf import OmegaConf
 from utils import tree_utils
 from optimizers.muon.muon import scale_by_muon, muon
-from optimizers.muon.muon_laprop import label_gpt
+from optimizers.muon.muon_laprop import label_gpt, muon_laprop
 from models import summarize_model_parmas
 from _src import init_language_model
 
@@ -73,9 +73,10 @@ def test_muon():
 
 
 def visualize_label_params():
+    """A visualization util function that structures the partition of muon_laprop."""
     model = init_language_model(config=OmegaConf.load("conf/model/gpt.yaml"), key=jax.random.PRNGKey(42))
     summary = summarize_model_parmas(model, verbose=False)
-    labels = jtu.tree_map_with_path(label_gpt, eqx.filter(model, eqx.is_array))
+    labels = label_gpt(eqx.filter(model, eqx.is_array))
 
     summary_list, _ = jtu.tree_flatten(summary)
     labels_list, _ = jtu.tree_flatten(labels)
@@ -84,6 +85,22 @@ def visualize_label_params():
     lines = [f"{s:<{max_len}} | {label}" for s, label in zip(summary_list, labels_list)]
 
     print("\n".join(lines))
+
+
+def test_muon_laprop():
+    model = init_language_model(config=OmegaConf.load("conf/model/gpt.yaml"), key=jax.random.PRNGKey(42))
+    params = eqx.filter(model, eqx.is_array)
+
+    optimizer = muon_laprop()
+    opt_state = optimizer.init(params)
+
+    for i in range(2):
+        grads = tree_utils.normalize(params)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        print(f"iter {i+1}\n  >> grads\n", jtu.tree_flatten(grads)[0])
+        print(f"  >> updates\n", jtu.tree_flatten(updates)[0])
+        print(f"  >> new params\n", jtu.tree_flatten(params)[0])
 
 
 if __name__ == "__main__":
