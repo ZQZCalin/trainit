9b2f07dc21e68742f677e8c93803cd46d470ccdd
Qinzi Zhang <zqz20100221@126.com>
2025-02-11 17:38:16 -0500

    implemented easy to config exps

--- scripts/experiments/preconditioners/mango_v2.sh
+++ scripts/experiments/preconditioners/mango_v2.sh
@@ -2,8 +2,8 @@
 
 #!/bin/bash
 
-project=null
-# project=pile_baseline
+# project=null
+project=pile_baseline
 log_data=False
 steps=2000
 batch_size=128
@@ -37,8 +37,23 @@ offset_beta=0.95
 
 
 # ...
-name="mangov2_test"
-lr_embedding=0.012
+# name="mango_wnorm_default"
+
+# ...
+# name="mango_wnorm_global_l2_p1"
+# scale_weight="l2|1"
+
+# ...
+# name="mango_wnorm_global_l2_p0.75"
+# scale_weight="l2|0.75"
+
+# ...
+# name="mango_wnorm_global_l2_p0.5"
+# scale_weight="l2|0.5"
+
+# ...
+# name="mango_wnorm_global_l2_p0.25"
+# scale_weight="l2|0.25"
 
 
 # ========================================================================
@@ -61,9 +76,11 @@ args=(
     "train.max_steps=$steps"
     "dataset.total_batch_size=$batch_size"
     "optimizer=$optimizer"
+    # global hyperparameters
     "optimizer.ns_steps=$ns_steps"
     "optimizer.eps=$eps"
     "optimizer.offset_beta=$offset_beta"
+    # lr schedule
     "optimizer/lr_config=$schedule"
     "optimizer.lr_config.warmup=$warmup"
     "optimizer.lr_config.const=$const"
@@ -76,28 +93,82 @@ args=(
     "$(parse "optimizer.lr.att_b" "lr_att_b")"
     "$(parse "optimizer.lr.vec_w" "lr_vec_w")"
     "$(parse "optimizer.lr.vec_b" "lr_vec_b")"
-    # global configs.
-    # if any of the following is defined, it will override 
-    # the dictionary-specific definition.
+    # beta1
+    "$(parse "optimizer.beta1.mat" "beta1_mat")"
+    "$(parse "optimizer.beta1.embedding" "beta1_embedding")"
+    "$(parse "optimizer.beta1.head" "beta1_head")"
+    "$(parse "optimizer.beta1.att_w" "beta1_att_w")"
+    "$(parse "optimizer.beta1.att_b" "beta1_att_b")"
+    "$(parse "optimizer.beta1.vec_w" "beta1_vec_w")"
+    "$(parse "optimizer.beta1.vec_b" "beta1_vec_b")"
+    # beta2
+    "$(parse "optimizer.beta2.mat" "beta2_mat")"
+    "$(parse "optimizer.beta2.embedding" "beta2_embedding")"
+    "$(parse "optimizer.beta2.head" "beta2_head")"
+    "$(parse "optimizer.beta2.att_w" "beta2_att_w")"
+    "$(parse "optimizer.beta2.att_b" "beta2_att_b")"
+    "$(parse "optimizer.beta2.vec_w" "beta2_vec_w")"
+    "$(parse "optimizer.beta2.vec_b" "beta2_vec_b")"
+    # nesterov
+    "$(parse "optimizer.nesterov.mat" "nesterov_mat")"
+    "$(parse "optimizer.nesterov.embedding" "nesterov_embedding")"
+    "$(parse "optimizer.nesterov.head" "nesterov_head")"
+    "$(parse "optimizer.nesterov.att_w" "nesterov_att_w")"
+    "$(parse "optimizer.nesterov.att_b" "nesterov_att_b")"
+    "$(parse "optimizer.nesterov.vec_w" "nesterov_vec_w")"
+    "$(parse "optimizer.nesterov.vec_b" "nesterov_vec_b")"
+    # use_adamw
+    "$(parse "optimizer.use_adamw.mat" "use_adamw_mat")"
+    "$(parse "optimizer.use_adamw.embedding" "use_adamw_embedding")"
+    "$(parse "optimizer.use_adamw.head" "use_adamw_head")"
+    "$(parse "optimizer.use_adamw.att_w" "use_adamw_att_w")"
+    "$(parse "optimizer.use_adamw.att_b" "use_adamw_att_b")"
+    "$(parse "optimizer.use_adamw.vec_w" "use_adamw_vec_w")"
+    "$(parse "optimizer.use_adamw.vec_b" "use_adamw_vec_b")"
+    # normalize
+    "$(parse "optimizer.normalize.mat" "normalize_mat")"
+    "$(parse "optimizer.normalize.embedding" "normalize_embedding")"
+    "$(parse "optimizer.normalize.head" "normalize_head")"
+    "$(parse "optimizer.normalize.att_w" "normalize_att_w")"
+    "$(parse "optimizer.normalize.att_b" "normalize_att_b")"
+    "$(parse "optimizer.normalize.vec_w" "normalize_vec_w")"
+    "$(parse "optimizer.normalize.vec_b" "normalize_vec_b")"
+    # scale_weight    
+    "$(parse "optimizer.scale_weight.mat" "scale_weight_mat")"
+    "$(parse "optimizer.scale_weight.embedding" "scale_weight_embedding")"
+    "$(parse "optimizer.scale_weight.head" "scale_weight_head")"
+    "$(parse "optimizer.scale_weight.att_w" "scale_weight_att_w")"
+    "$(parse "optimizer.scale_weight.att_b" "scale_weight_att_b")"
+    "$(parse "optimizer.scale_weight.vec_w" "scale_weight_vec_w")"
+    "$(parse "optimizer.scale_weight.vec_b" "scale_weight_vec_b")"
+    # Global configs.
+    # If any of the following is defined, it will override 
+    # the dictionary-specific definition to a float.
     "$(parse "optimizer.lr" "lr")"
+    "$(parse "optimizer.beta1" "beta1")"
+    "$(parse "optimizer.beta2" "beta2")"
+    "$(parse "optimizer.nesterov" "nesterov")"
+    "$(parse "optimizer.use_adamw" "use_adamw")"
+    "$(parse "optimizer.normalize" "normalize")"
+    "$(parse "optimizer.scale_weight" "scale_weight")"
 )
 
-python main.py ${args[@]}
-
-# qsub <<EOF
-# #!/bin/bash -l
-
-# #$ -pe omp 8
-# #$ -l gpus=1
-# #$ -l gpu_type=L40S     # Specifies the gpu type.
-# #$ -l h_rt=8:00:00      # Specifies the hard time limit for the job
-# #$ -N "$name".sh
-# #$ -o $OUTPUT_PATH/\$JOB_NAME.o\$JOB_ID     # Escape environment variables with \$
-# #$ -e $OUTPUT_PATH/\$JOB_NAME.e\$JOB_ID
-
-# sleep $(((RANDOM % 1000) / 100))   # Prevents simultaneous reads of loadit dataset
-
-# source activate_env.sh
 # python main.py ${args[@]}
-# EOF
-# echo "Submitted job: $name"
+
+qsub <<EOF
+#!/bin/bash -l
+
+#$ -pe omp 8
+#$ -l gpus=1
+#$ -l gpu_type=L40S     # Specifies the gpu type.
+#$ -l h_rt=8:00:00      # Specifies the hard time limit for the job
+#$ -N "$name".sh
+#$ -o $OUTPUT_PATH/\$JOB_NAME.o\$JOB_ID     # Escape environment variables with \$
+#$ -e $OUTPUT_PATH/\$JOB_NAME.e\$JOB_ID
+
+sleep $(((RANDOM % 1000) / 100))   # Prevents simultaneous reads of loadit dataset
+
+source activate_env.sh
+python main.py ${args[@]}
+EOF
+echo "Submitted job: $name"
