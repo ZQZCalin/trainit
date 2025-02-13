fc38867af39abed863e544ec47d90f423592119d
Qinzi Zhang <zqz20100221@126.com>
2025-02-12 23:06:48 -0500

    new experiments of mango, testing scale_by_weight_norm

--- scripts/experiments/preconditioners/mango_v2.sh
+++ scripts/experiments/preconditioners/mango_v2.sh
@@ -4,6 +4,7 @@
 
 # project=null
 project=pile_baseline
+
 log_data=False
 steps=2000
 batch_size=128
@@ -34,7 +35,35 @@ optimizer=mango_v2
 ns_steps=6
 eps=1e-8
 offset_beta=0.95
+scale_clip_low=1.0
+scale_clip_high=null
 
+# Normalization defaults
+normalize_mat="ns"
+normalize_embedding=null
+normalize_head="ns"
+normalize_attn_w="ns"
+normalize_attn_b="l2"
+normalize_vec_w=null
+normalize_vec_b="l2"
+
+# Scale by weight defaults
+# defaults to same.
+# you can turn it off by uncommenting the last line
+scale_weight_mat="op"
+scale_weight_embedding=null
+scale_weight_head="op"
+scale_weight_attn_w="op"
+scale_weight_attn_b="l2"
+scale_weight_vec_w=null
+scale_weight_vec_b="l2"
+# scale_weight=null
+
+
+
+# ========================================================================
+# Experiments start here
+# ========================================================================
 
 # ...
 # aka "mango_wnorm_same_p1"
@@ -86,6 +115,25 @@ offset_beta=0.95
 # name="mango_adamw_nones_lr${lr}"
 
 
+# Feb.12: Experiments with corrected implementation of scale_by_weight_norm
+# clips weight_norm
+beta1=0.95
+beta2=0.95
+nesterov=True
+use_adamw=False
+
+# scale_power=1
+# lr=0.01
+# # lr=3e-3
+# # lr=1e-3
+# # lr=3e-4
+# # lr=1e-4
+# scale_power=0.75
+# scale_power=0.5
+# scale_power=0.25
+# name="mango_scale_p${scale_power}_lr${lr}"
+
+
 # ========================================================================
 # Below is submit function. Only change the part related to global
 # vs param-wise learning rate.
@@ -99,6 +147,7 @@ parse() {
     fi
 }
 
+# project=null
 args=(
     "logging.wandb_project=$project"
     "logging.wandb_name=$name"
@@ -112,6 +161,8 @@ args=(
     "optimizer.ns_steps=$ns_steps"
     "optimizer.eps=$eps"
     "optimizer.offset_beta=$offset_beta"
+    "optimizer.scale_clip_low=$scale_clip_low"
+    "optimizer.scale_clip_high=$scale_clip_high"
     # lr schedule
     "optimizer/lr_config=$schedule"
     "optimizer.lr_config.warmup=$warmup"
@@ -121,64 +172,64 @@ args=(
     "$(parse "optimizer.lr.mat" "lr_mat")"
     "$(parse "optimizer.lr.embedding" "lr_embedding")"
     "$(parse "optimizer.lr.head" "lr_head")"
-    "$(parse "optimizer.lr.att_w" "lr_att_w")"
-    "$(parse "optimizer.lr.att_b" "lr_att_b")"
+    "$(parse "optimizer.lr.attn_w" "lr_attn_w")"
+    "$(parse "optimizer.lr.attn_b" "lr_attn_b")"
     "$(parse "optimizer.lr.vec_w" "lr_vec_w")"
     "$(parse "optimizer.lr.vec_b" "lr_vec_b")"
     # beta1
     "$(parse "optimizer.beta1.mat" "beta1_mat")"
     "$(parse "optimizer.beta1.embedding" "beta1_embedding")"
     "$(parse "optimizer.beta1.head" "beta1_head")"
-    "$(parse "optimizer.beta1.att_w" "beta1_att_w")"
-    "$(parse "optimizer.beta1.att_b" "beta1_att_b")"
+    "$(parse "optimizer.beta1.attn_w" "beta1_attn_w")"
+    "$(parse "optimizer.beta1.attn_b" "beta1_attn_b")"
     "$(parse "optimizer.beta1.vec_w" "beta1_vec_w")"
     "$(parse "optimizer.beta1.vec_b" "beta1_vec_b")"
     # beta2
     "$(parse "optimizer.beta2.mat" "beta2_mat")"
     "$(parse "optimizer.beta2.embedding" "beta2_embedding")"
     "$(parse "optimizer.beta2.head" "beta2_head")"
-    "$(parse "optimizer.beta2.att_w" "beta2_att_w")"
-    "$(parse "optimizer.beta2.att_b" "beta2_att_b")"
+    "$(parse "optimizer.beta2.attn_w" "beta2_attn_w")"
+    "$(parse "optimizer.beta2.attn_b" "beta2_attn_b")"
     "$(parse "optimizer.beta2.vec_w" "beta2_vec_w")"
     "$(parse "optimizer.beta2.vec_b" "beta2_vec_b")"
     # nesterov
     "$(parse "optimizer.nesterov.mat" "nesterov_mat")"
     "$(parse "optimizer.nesterov.embedding" "nesterov_embedding")"
     "$(parse "optimizer.nesterov.head" "nesterov_head")"
-    "$(parse "optimizer.nesterov.att_w" "nesterov_att_w")"
-    "$(parse "optimizer.nesterov.att_b" "nesterov_att_b")"
+    "$(parse "optimizer.nesterov.attn_w" "nesterov_attn_w")"
+    "$(parse "optimizer.nesterov.attn_b" "nesterov_attn_b")"
     "$(parse "optimizer.nesterov.vec_w" "nesterov_vec_w")"
     "$(parse "optimizer.nesterov.vec_b" "nesterov_vec_b")"
     # use_adamw
     "$(parse "optimizer.use_adamw.mat" "use_adamw_mat")"
     "$(parse "optimizer.use_adamw.embedding" "use_adamw_embedding")"
     "$(parse "optimizer.use_adamw.head" "use_adamw_head")"
-    "$(parse "optimizer.use_adamw.att_w" "use_adamw_att_w")"
-    "$(parse "optimizer.use_adamw.att_b" "use_adamw_att_b")"
+    "$(parse "optimizer.use_adamw.attn_w" "use_adamw_attn_w")"
+    "$(parse "optimizer.use_adamw.attn_b" "use_adamw_attn_b")"
     "$(parse "optimizer.use_adamw.vec_w" "use_adamw_vec_w")"
     "$(parse "optimizer.use_adamw.vec_b" "use_adamw_vec_b")"
     # normalize
     "$(parse "optimizer.normalize.mat" "normalize_mat")"
     "$(parse "optimizer.normalize.embedding" "normalize_embedding")"
     "$(parse "optimizer.normalize.head" "normalize_head")"
-    "$(parse "optimizer.normalize.att_w" "normalize_att_w")"
-    "$(parse "optimizer.normalize.att_b" "normalize_att_b")"
+    "$(parse "optimizer.normalize.attn_w" "normalize_attn_w")"
+    "$(parse "optimizer.normalize.attn_b" "normalize_attn_b")"
     "$(parse "optimizer.normalize.vec_w" "normalize_vec_w")"
     "$(parse "optimizer.normalize.vec_b" "normalize_vec_b")"
     # scale_weight
     "$(parse "optimizer.scale_weight.mat" "scale_weight_mat")"
     "$(parse "optimizer.scale_weight.embedding" "scale_weight_embedding")"
     "$(parse "optimizer.scale_weight.head" "scale_weight_head")"
-    "$(parse "optimizer.scale_weight.att_w" "scale_weight_att_w")"
-    "$(parse "optimizer.scale_weight.att_b" "scale_weight_att_b")"
+    "$(parse "optimizer.scale_weight.attn_w" "scale_weight_attn_w")"
+    "$(parse "optimizer.scale_weight.attn_b" "scale_weight_attn_b")"
     "$(parse "optimizer.scale_weight.vec_w" "scale_weight_vec_w")"
     "$(parse "optimizer.scale_weight.vec_b" "scale_weight_vec_b")"
     # scale_power
     "$(parse "optimizer.scale_power.mat" "scale_power_mat")"
     "$(parse "optimizer.scale_power.embedding" "scale_power_embedding")"
     "$(parse "optimizer.scale_power.head" "scale_power_head")"
-    "$(parse "optimizer.scale_power.att_w" "scale_power_att_w")"
-    "$(parse "optimizer.scale_power.att_b" "scale_power_att_b")"
+    "$(parse "optimizer.scale_power.attn_w" "scale_power_attn_w")"
+    "$(parse "optimizer.scale_power.attn_b" "scale_power_attn_b")"
     "$(parse "optimizer.scale_power.vec_w" "scale_power_vec_w")"
     "$(parse "optimizer.scale_power.vec_b" "scale_power_vec_b")"
     # Global configs.
