009df0702da511827556460087c5e9ba09fc9ba2
Qinzi Zhang <zqz20100221@126.com>
2025-03-31 15:05:10 -0400

    baseline exp

--- scripts/train_lm.sh
+++ scripts/train_lm.sh
@@ -38,34 +38,65 @@
 #     checkpoint.save_steps=$save_steps
 
 
-project=null
-name=normalized_sgdm
-log_data=True
-steps=2000
+
+project=greedy_lr_schedule
+name=baseline
 batch_size=128
+use_amp=False
+log_data=False
+seed=42
 
-optimizer=normalized_sgdm
-lr=0.05
-momentum=0.95
-nesterov=True
-normalize=layer
+optimizer=adamw
+b1=0.9
+b2=0.999
+wd=0.1
+nesterov=False
 
+lr=1e-3
+steps=2000
 schedule=linear
 warmup=200
 wait=0
+
+# System variables
+BASE_DIR=/projectnb/aclab/qinziz/trainit
+DATE=$(date +"%Y-%m-%d")
+OUTPUT_PATH=$BASE_DIR/scc_outputs/$DATE/$name
+
+mkdir -p $OUTPUT_PATH
+
+qsub <<EOF
+#!/bin/bash -l
+
+#$ -pe omp 8
+#$ -l gpus=1
+#$ -l gpu_type=L40S
+#$ -l h_rt=8:00:00
+#$ -N $name
+#$ -o $OUTPUT_PATH/\$JOB_NAME.o\$JOB_ID
+#$ -e $OUTPUT_PATH/\$JOB_NAME.e\$JOB_ID
+
+source activate_env.sh
+
 python main.py \
     logging.wandb_project=$project \
-    logging.wandb_name="$name"_lr:"$lr" \
+    logging.wandb_name=\$JOB_NAME \
+    logging.wandb_runid=\$JOB_ID \
+    logging.wandb_expname=$name \
     logging.log_callback_data=$log_data \
     train.max_steps=$steps \
+    train.use_amp=$use_amp \
     dataset.total_batch_size=$batch_size \
+    random_seed=$seed \
     optimizer=$optimizer \
-    optimizer.momentum=$momentum \
-    optimizer.nesterov=$nesterov \
-    optimizer.normalize=$normalize \
+    optimizer.beta1=$b1 \
+    optimizer.beta2=$b2 \
+    optimizer.weight_decay=$wd \
+    optimizer.use_nesterov=$nesterov \
     optimizer/lr_config=$schedule \
     optimizer.lr_config.lr=$lr \
     optimizer.lr_config.warmup=$warmup \
     optimizer.lr_config.const=$wait \
-    optimizer.lr_config.max_steps=$steps \
-    optimizer/wrapper=adamw_2dmask
+    optimizer.lr_config.max_steps=$steps
+EOF
+echo "Submitted job: $name"
